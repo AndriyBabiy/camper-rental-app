e0c31e32b11730c20a73c1f066a4a1278e4b04cd
Andriy Babiy <andriybabiy97@gmail.com>
2024-05-19 12:52:25 +0200

    Deploy adjustments

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/camper-select">
+      <BrowserRouter basename="/camper-rental-app">
         <ThemeProvider theme={theme}>
           <GlobalStyles />
           <App />
